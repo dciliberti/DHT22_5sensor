0286b6e83b77bfa5c913855372e40be6f44e6c30
Danilo Ciliberti <52099779+dciliberti@users.noreply.github.com>
2019-08-14 19:35:53 +0200

    Added license boilerplate

--- DHT22_5sensor.ino
+++ DHT22_5sensor.ino
@@ -1,3 +1,21 @@
+/*
+    DHT22_5sensor.ino - Arduino code for the readings of temperature and humidity sensors.
+    Copyright (C) 2018  Danilo Ciliberti dancili@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 // Libraries
 #include <DHT.h>
 
